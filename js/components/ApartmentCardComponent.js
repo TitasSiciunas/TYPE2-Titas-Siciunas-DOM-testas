3b142e1ba867c0b37fc873d3a57bab9fb1675c0a
Titas Sičiūnas <titas.siciunas@gmail.com>
2021-11-10 10:34:28 +0200

    [7]: loading animation and card rendering

---
+++ js/components/ApartmentCardComponent.js
@@ -0,0 +1,12 @@
+class ApartmentCardComponent {
+  constructor(props) {
+    this.props = props;
+    this.init();
+  }
+
+  init = () => {
+    this.htmlElement = document.createElement('article');
+    this.htmlElement.className = 'card shadow p-3'
+    this.htmlElement.innerHTML = 'Apartment';
+  }
+}
--- js/components/ApartmentGridComponent.js
+++ js/components/ApartmentGridComponent.js
@@ -17,20 +17,33 @@ class ApartmentGridComponent {
 
   fetchApartments = () => API.fetchApartments(this.saveApartments, this.showError);
 
+  wrapChild = element => {
+    const wrapper = document.createElement('div');
+    wrapper.className = 'col-12, col-sm-6, col-lg-4, col-xl-3';
+    wrapper.append(element);
+    return wrapper;
+  }
+
   init = () => {
     this.state.loading = true;
-    this.fetchApartments();
+    setTimeout(this.fetchApartments, 1000);
     this.htmlElement = document.createElement('div');
+    this.htmlElement.className = 'row g-3'
 
     this.render();
   }
 
   render = () => {
-    const { loading } = this.state;
+    const { loading, apartments } = this.state;
     if (loading) {
-      this.htmlElement.innerHTML = 'sunčiama...'
+      this.htmlElement.innerHTML = '<div class="text-center"><img src="assets/loading.gif" /></div>';
     } else {
-      this.htmlElement.innerHTML = 'parsiųsta!';
+      this.htmlElement.innerHTML = '';
+      const children = apartments
+      .map(x => new ApartmentCardComponent(x))
+      .map(x => x.htmlElement)
+      .map(this.wrapChild)
+      this.htmlElement.append(...children);
     }
   }
 
