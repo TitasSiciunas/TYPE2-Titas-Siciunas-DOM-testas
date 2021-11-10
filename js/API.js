5e1a453dc0e46f825c6ade19a8fa14cb0ba5d937
Titas Sičiūnas <titas.siciunas@gmail.com>
2021-11-10 09:21:02 +0200

    [4]: API file created and [2]: fix

---
+++ js/API.js
@@ -0,0 +1,27 @@
+const baseURL = 'http://localhost:3000';
+
+class API {
+  static fetchApartments = (success, failure) => {
+    fetch(`${baseURL}/apartments`)
+      .then(res => res.json())
+      .then(success)
+      .catch(failure)
+  }
+
+  static deleteApartment = (id, success, failure) => {
+    fetch(`${baseURL}/apartments/${id}`, { method: 'DELETE' })
+      .then(success)
+      .catch(failure)
+  }
+}
+
+API.fetchApartments(
+  (duomenys) => console.log('Duomenys gauti', duomenys),
+  (klaida) => console.error('Klaida', klaida)
+)
+
+API.deleteApartment(
+  '1',
+  (duomenys) => console.log('Ištrinta', duomenys),
+  (klaida) => console.error('Klaida', klaida)
+)
